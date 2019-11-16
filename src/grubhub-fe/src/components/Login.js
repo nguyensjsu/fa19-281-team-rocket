15ba1e9ca55f284b55fa6902a1a7b55cab2ff10c
Megha <megha@sjsu.edu>
2019-11-15 17:49:05 -0800

    Login and Signup code

---
+++ src/grubhub-fe/src/components/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import "../App.css";
+
+class Login extends Component {
+  state = {};
+  render() {
+    return (
+      <Form className="login-page">
+        <h1 className="text-center" style={{ color: "red" }}>
+          <span className="font-weight-bold">GrubHub</span>
+        </h1>
+        <h2 className="text-center">Login</h2>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email"></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password"></Input>
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block">Login</Button>
+        <div className="text-center pt-3">New to GrubHub?</div>
+        <div className="text-center">
+          <a href="/signup">Signup</a>
+        </div>
+      </Form>
+    );
+  }
+}
+
+export default Login;
