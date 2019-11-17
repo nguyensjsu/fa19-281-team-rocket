d29e965f1d9fb9eb72745cde26d6dd6475ad5dd0
Jojo Joseph <jojo94.mec@gmail.com>
2019-11-16 21:02:53 -0800

    Inventory page

---
+++ src/grubhub-fe/src/components/Item.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import {  Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Row, Col, Container } from 'reactstrap';
+import "../App.css";
+import axios from 'axios';
+
+class Item extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        console.log(this.props)
+        return (
+        <Col sm="4">
+        <Card>
+        <CardImg top width="100%" src={this.props.item.Image} alt="Card image cap" />
+        <CardBody>
+        <CardTitle>{this.props.item.Name}</CardTitle>
+        <CardSubtitle>{this.props.item.Category}</CardSubtitle>
+        <CardBody>{this.props.item.Description}</CardBody>
+        <CardText>{this.props.item.Price} Dollars</CardText>
+         <Button>Add to Cart</Button>
+        </CardBody>
+      </Card>
+      </Col>
+        );
+    }
+}
+
+class Inventory extends Component {
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://9tz63arnh5.execute-api.us-west-2.amazonaws.com/prod/inventory`)
+          .then(res => {
+            const items = res.data;
+            console.log(items);
+            this.setState({ items });
+          })
+    }
+
+    render() {
+        console.log("State : " + this.state.items)
+        return (
+            <Container fluid>
+                <Row>
+                {this.state.items.map(item => <Item item={item}/>)}
+            </Row>
+            </Container>
+        )
+    }
+
+}
+
+export default Inventory;
--- src/grubhub-fe/src/components/Main.js
+++ src/grubhub-fe/src/components/Main.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import User from "./User";
+import Inventory from "./Item";
 
 class Main extends Component {
   state = {};
@@ -12,6 +13,7 @@ class Main extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/user" component={User} />
+        <Route path="/inventory" component={Inventory} />
       </div>
     );
   }
