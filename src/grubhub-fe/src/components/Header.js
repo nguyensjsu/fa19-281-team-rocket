9e427e45562ed2c7c48c9f2e239431ebe52fcc35
harshrajm <harshrajm@gmail.com>
2019-11-21 18:32:58 -0800

    UI redirecting to payment page

--- src/grubhub-fe/src/components/Header.js
+++ src/grubhub-fe/src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Table } from "reactstrap";
+import { Redirect } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import {
   Navbar,
   Nav,
@@ -12,6 +14,7 @@ import {
 } from "reactstrap";
 import "../App.css";
 import { ROOT_URL } from "../config/URLSettings";
+import Payment from "./Payment";
 
 class Header extends Component {
   constructor(props) {
@@ -21,19 +24,15 @@ class Header extends Component {
       item: "",
       itemSubTotal: 0,
       price: 0,
-      orderedItems: []
+      orderedItems: [],
+      redirectToPayments: false
     };
   }
 
   componentDidMount() {
     axios.get(ROOT_URL + `cartItems/sam.mam@gmail.com`).then(response => {
       var data = response.data;
-      console.log("Item", data[0].item);
-      // this.setState({quantity:data[0].quantity})
-      // this.setState({item:data[0].item})
-      // this.setState({price :data[0].price })
-      // var totalSubtotal =  data[0].price * data[0].quantity
-      // this.setState({itemSubTotal:totalSubtotal})
+      console.log("Item", data);
       var itemSubTotal = 0;
       data.map(v => {
         var intprice = parseInt(v.price);
@@ -51,12 +50,7 @@ class Header extends Component {
     console.log("Calling getCart items api");
     axios.get(ROOT_URL + `cartItems/sam.mam@gmail.com`).then(response => {
       var data = response.data;
-      console.log("Item", data[0].item);
-      // this.setState({quantity:data[0].quantity})
-      // this.setState({item:data[0].item})
-      // this.setState({price :data[0].price })
-      // var totalSubtotal =  data[0].price * data[0].quantity
-      // this.setState({itemSubTotal:totalSubtotal})
+      console.log("Item", data);
       var itemSubTotal = 0;
       data.map(v => {
         var intprice = parseInt(v.price);
@@ -70,6 +64,15 @@ class Header extends Component {
     });
   };
 
+  handleCheckout = orderedItems => {
+    console.log("orderedItems", orderedItems);
+    //localStorage.setItem("emailId"
+    //this.props.history.push("/payment");
+    this.setState({ redirectToPayments: true });
+    this.setState({ orderedItems });
+    //return <Redirect to="/payment" />;
+  };
+
   render() {
     let items = this.state.orderedItems.map(oitem => {
       return (
@@ -81,36 +84,56 @@ class Header extends Component {
       );
     });
     return (
-      <div className="header">
-        <Navbar color="" light expand="md">
-          <h1 style={{ color: "red" }}>
-            <span className="font-weight-bold">GrubHub</span>
-          </h1>
-          <Nav className="ml-auto" navbar>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret onClick={this.getCartItems}>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <h6 className="text-center">Your Orders</h6>
-                <DropdownItem divider />
+      <React.Fragment>
+        {this.state.redirectToPayments && (
+          <Redirect
+            to={{
+              pathname: "/payment",
+              state: {
+                orderedItems: this.state.orderedItems,
+                itemSubTotal: this.state.itemSubTotal
+              }
+            }}
+          />
+        )}
+        <div className="header">
+          <Navbar color="" light expand="md">
+            <h1 style={{ color: "red" }}>
+              <span className="font-weight-bold">GrubHub</span>
+            </h1>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret onClick={this.getCartItems}>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <h6 className="text-center">Your Orders</h6>
+                  <DropdownItem divider />
 
-                <tbody>{items}</tbody>
+                  <tbody>{items}</tbody>
 
-                <DropdownItem divider />
-                <DropdownItem>
-                  Item Subtotals&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $
-                  {this.state.itemSubTotal}
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  <Button color="success">Proceed to Checkout</Button>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Navbar>
-      </div>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Item Subtotals&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $
+                    {this.state.itemSubTotal}
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <Button
+                      onClick={() =>
+                        this.handleCheckout(this.state.orderedItems)
+                      }
+                      color="success"
+                    >
+                      Proceed to Checkout
+                    </Button>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Navbar>
+        </div>
+      </React.Fragment>
     );
   }
 }
