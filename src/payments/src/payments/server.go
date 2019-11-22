4ac94584ed968dce1d503ea35a37836f71fbdb44
deepikay912 <deepika640yannamani@gmail.com>
2019-11-21 18:08:01 -0800

    cors error

--- src/payments/src/payments/server.go
+++ src/payments/src/payments/server.go
@@ -206,6 +206,5 @@ func getPaymentsHandler(formatter *render.Render) http.HandlerFunc {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*") 
 }
