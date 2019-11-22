54404954e8b4819e4828f07132b07ecd6cb636e0
Megha <megha@sjsu.edu>
2019-11-22 12:38:12 -0800

    User js

--- src/grubhub-fe/src/components/User.js
+++ src/grubhub-fe/src/components/User.js
@@ -1,40 +1,74 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Input, Label, Table } from "reactstrap";
 import "../App.css";
+import axios from "axios";
 
 class User extends Component {
-  state = {};
+  state = {
+    orders: []
+  };
+
+  componentWillMount() {
+    let email = localStorage.getItem("emailId");
+    axios
+      .get("http://34.219.240.229:8080/allOrdersByEmail/" + email)
+      .then(response => {
+        console.log("Status Code : ", response.status);
+        if (response.status === 200) {
+          this.setState({
+            orders: response.data
+          });
+        }
+      });
+  }
   render() {
-    return (
-      <Form className="login-page">
-        <h1 className="text-center" style={{ color: "red" }}>
-          <span className="font-weight-bold">R-Tea</span>
-        </h1>
-        <h2 className="text-center">Welcome</h2>
-        <Table>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Form>
-    );
+    let orders = this.state.orders;
+    let name = localStorage.getItem("name");
+    if (orders != null) {
+      return (
+        <div>
+          <Form className="login-page">
+            <h1 className="text-center" style={{ color: "red" }}>
+              <span className="font-weight-bold">R-Tea</span>
+            </h1>
+            <h2 className="text-center">Welcome {name}</h2>
+          </Form>
+          <div className="text-center">
+            <Table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>OrderID</th>
+                  <th>Status</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.orders.map(order => (
+                  <tr>
+                    <th scope="row">*</th>
+                    <td>{order["_id"]}</td>
+                    <td>{order["orderStatus"] ? "Success" : "Failed"}</td>
+                    <td>{Date(order["orderPlacedTime"].toString())}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      );
+    } else {
+      let name = localStorage.getItem("name");
+      return (
+        <Form className="login-page">
+          <h1 className="text-center" style={{ color: "red" }}>
+            <span className="font-weight-bold">R-Tea</span>
+          </h1>
+          <h2 className="text-center">Welcome {name}</h2>
+          <h3 className="text-center">No Orders Found!!!</h3>
+        </Form>
+      );
+    }
   }
 }
 
